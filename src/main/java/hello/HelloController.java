026d587c7a113797c923f4cef58e9d61f44037ee
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-02-26 15:34:12 -0500

    init commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @GetMapping
+	@RequestMapping(value="/getPersonalInfo")
+	public List<String> getPersonalInfo() {
+		List<String> personalDetails = new ArrayList<String>();
+		personalDetails.add("Mitchell Irvin");
+		personalDetails.add("Alma Mater: University of Florida");
+		personalDetails.add("Hobbies: Reading, Lifting, Learning");
+		return personalDetails; 
+	}
+    
+}
