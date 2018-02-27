7a6ef2d1ffe9857e9622b12550a372903be541ea
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-02-26 19:45:55 -0500

    enabled CORS for localhost:4200

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +17,7 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
 	@RequestMapping(value="/getPersonalInfo")
 	public List<String> getPersonalInfo() {
